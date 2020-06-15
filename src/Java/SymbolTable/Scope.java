d70e3b2ef8a9b7cca67915c3c1054d1e5b83c70b
Jehadab <jehad1996@hotmail.com>
2020-06-15 21:54:17 +0300

    fix Subfunction_body add Symbol table classes

--- src/Java/SymbolTable/Scope.java
+++ src/Java/SymbolTable/Scope.java
@@ -1,7 +1,38 @@
 package Java.SymbolTable;
 
-/**
- * Created by Jehad on 6/15/2020.
- */
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Scope {
+    private String id;
+    private Scope parent;
+    private Map<String, Symbol> symbolMap = new LinkedHashMap< String , Symbol >();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public Scope getParent() {
+        return parent;
+    }
+
+    public void setParent(Scope parent) {
+        this.parent = parent;
+    }
+
+    public Map<String, Symbol> getSymbolMap() {
+        return symbolMap;
+    }
+
+    public void setSymbolMap(Map<String, Symbol> symbolMap) {
+        this.symbolMap = symbolMap;
+    }
+    public void addSymbol(String name, Symbol symbol) {
+        this.symbolMap.put(name,symbol);
+    }
 }
